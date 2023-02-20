3424311e2d96fca7dd4b9b2e860b875d2244cbbe
lolgitech <Lolgitech@users.noreply.github.com>
2023-02-20 11:07:37 +0100

    first commit

---
+++ memegenerator/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Header from "./components/header.jsx"
+import Meme from "./components/meme.jsx"
+
+
+
+export function App() {
+ 
+
+  return (
+    <div className="App">
+      
+      <Header />
+      <Meme />
+       
+    </div>
+  )
+}
+
+export default App
---
+++ memegenerator/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+    return(
+        <header className="header">
+        <img
+            src=""
+            className="header--img"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React course - Project 3"</h4>
+
+        </header>
+    )
+    
+
+}
